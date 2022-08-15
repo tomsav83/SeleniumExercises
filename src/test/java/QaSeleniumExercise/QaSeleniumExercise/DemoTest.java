f04168f1dde33576cb380273f364f6e6f2369766
Tom S <tom@tom.com>
2022-08-15 13:06:35 +0100

    added select dress using add to cart button

--- src/test/java/QaSeleniumExercise/QaSeleniumExercise/DemoTest.java
+++ src/test/java/QaSeleniumExercise/QaSeleniumExercise/DemoTest.java
@@ -1,5 +1,7 @@
 package QaSeleniumExercise.QaSeleniumExercise;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DemoTest {
 	
@@ -16,8 +21,12 @@ public class DemoTest {
 	@BeforeEach
 	void setup() {
 		this.driver = new ChromeDriver();
+//		this.driver.manage().window().maximize();
+		
+//		this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 	}
+	
 	
 	
 	@Test
@@ -28,6 +37,14 @@ public class DemoTest {
 		search.sendKeys(Keys.ENTER);
 		
 		this.driver.findElement(By.cssSelector("#searchbox > button")).click();
+		
+		this.driver.findElement(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default")).click();
+		
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//		
+//		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a")));
+		
+		this.driver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a")).click();
 		
 		
 	}
