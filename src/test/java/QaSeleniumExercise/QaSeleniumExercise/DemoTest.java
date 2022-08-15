0634c52cf2f6a6bb2fd1921f7ba9271ed2dd8e5a
Tom S <tom@tom.com>
2022-08-15 16:10:12 +0100

    completed taking product through checkout process

--- src/test/java/QaSeleniumExercise/QaSeleniumExercise/DemoTest.java
+++ src/test/java/QaSeleniumExercise/QaSeleniumExercise/DemoTest.java
@@ -21,9 +21,9 @@ public class DemoTest {
 	@BeforeEach
 	void setup() {
 		this.driver = new ChromeDriver();
-//		this.driver.manage().window().maximize();
+		this.driver.manage().window().maximize();
 		
-//		this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // forces execution of test stages to wait for 20 seconds 
 		
 	}
 	
@@ -32,19 +32,53 @@ public class DemoTest {
 	@Test
 	void demoTest() {
 		this.driver.get("http://automationpractice.com/index.php");
-		WebElement search = this.driver.findElement(By.cssSelector("#search_query_top"));
-		search.sendKeys("Dress");
-		search.sendKeys(Keys.ENTER);
+		WebElement search = this.driver.findElement(By.cssSelector("#search_query_top")); // selects search input field
+		search.sendKeys("Dress"); // inputs "Dress" into search bar
+		search.sendKeys(Keys.ENTER); // automates pressing enter
 		
-		this.driver.findElement(By.cssSelector("#searchbox > button")).click();
+		this.driver.findElement(By.cssSelector("#searchbox > button")).click(); // targets searchbox click button and executes 
 		
-		this.driver.findElement(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default")).click();
 		
-//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//		
-//		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a")));
+		this.driver.findElement(By.cssSelector
+				("#center_column > ul > li:nth-child(1) > div > div.left-block > div > a.product_img_link")).click(); // selects the product and executes 
 		
-		this.driver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a")).click();
+		this.driver.findElement(By.cssSelector
+				("#add_to_cart > button")).click(); // selects add to cart button and executes
+		
+		this.driver.findElement(By.cssSelector
+				("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a")).click(); // selects proceed to checkout and executes - onto summary page
+		
+		this.driver.findElement(By.cssSelector
+				("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium")).click(); // selects proceed to checkout and executes - onto sign in page
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#SubmitLogin")));
+		
+		WebElement email = this.driver.findElement(By.cssSelector("#email")); // selects email input field
+		email.sendKeys("tom@me.com"); // inputs "tom@me.com" into email field
+		
+		WebElement password = this.driver.findElement(By.cssSelector("#passwd")); // selects password input field
+		password.sendKeys("password"); // inputs "password" into email field
+		
+		this.driver.findElement(By.cssSelector
+				("#SubmitLogin")).click(); // targets the sign in button and executes - onto address page
+		
+		
+		this.driver.findElement(By.cssSelector
+				("#center_column > form > p > button")).click();
+		
+		this.driver.findElement(By.cssSelector
+				("#cgv")).click(); // checks the T&C's checkbox
+		
+		this.driver.findElement(By.cssSelector
+				("#form > p > button")).click(); // targets the proceed to checkout and executes
+		
+		this.driver.findElement(By.cssSelector
+				("#HOOK_PAYMENT > div:nth-child(1) > div > p > a")).click(); // selects and executes the "Pay by bank wire" element - onto "ORDER SUMMARY - BANK WIRE PAYMENT"
+		
+		this.driver.findElement(By.cssSelector
+				("#cart_navigation > button")).click(); // selects the "confirm order" and executes - completes order
 		
 		
 	}
